d87e54025b0f5393d9489df9742f9d9c34c01f73
0carloscastillo0 <carlos.castillo.d@usach.cl>
2020-10-30 17:57:02 -0300

    hice el comentario en python

--- tombola.py
+++ tombola.py
@@ -6,3 +6,5 @@ print ("El primero que realizará el control de ingeso:  ",rdm.choice (lista))
 //Esta es mi linea de test ATTE El Cevas
 
 // Linea del pana Deivid uwu
+
+#Confirmo , el comentario era gato , linea de 0carloscastillo0
