c9acce222b11c2de3fd52c56c7e2fa82becdb6e4
martinormazabal <martin.ormazabal@usach.cl>
2020-10-30 17:57:42 -0300

    [Martín] agrega más línea"

--- tombola.py
+++ tombola.py
@@ -6,3 +6,8 @@ print ("El primero que realizará el control de ingeso:  ",rdm.choice (lista))
 //Esta es mi linea de test ATTE El Cevas
 
 // Linea del pana Deivid uwu
+
+//Línea de Martín
+
+lista2 = ["Hola","Ese es un mensaje","Merge"]
+print (rdm.choice(lista2))
