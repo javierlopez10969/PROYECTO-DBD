028f7a05396e7e203a162668fb44c77cf4143be8
Sebastian Astete Ramirez <sebastian.astete.r@usach.cl>
2021-01-16 22:17:39 -0300

    Controladores de TODAS LAS TABLAS - vacias

--- PROYECTO-DBD/vendor/composer/autoload_real.php
+++ PROYECTO-DBD/vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInita653a1a886d5865b4c03f0ff86b4bcba
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInita653a1a886d5865b4c03f0ff86b4bcba', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInita653a1a886d5865b4c03f0ff86b4bcba', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInita653a1a886d5865b4c03f0ff86b4bcba::getInitializer($loader));
         } else {
