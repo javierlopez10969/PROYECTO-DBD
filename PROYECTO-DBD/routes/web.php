a9c5e3aac339ac851beea58733bac2b33dc734a7
carlos <carlos.castillo.d@usach.cl>
2021-01-17 14:59:28 -0300

    Edicion de controllers con validaciones y Rutas de Carlos

--- PROYECTO-DBD/routes/web.php
+++ PROYECTO-DBD/routes/web.php
@@ -46,3 +46,39 @@ Route::get('/feria_puestosDeVenta/{id}','Feria_PuestoDeVenta@show');
 Route::post('/feria_puestosDeVenta/create','Feria_PuestoDeVenta@store');
 Route::put('/feria_puestosDeVenta/update/{id}','Feria_PuestoDeVenta@update');
 Route::delete('/feria_puestosDeVenta/delete/{id}','Feria_PuestoDeVenta@destroy');
+
+//RUTAS DE CONTROLLERS DE CARLOS
+//Rutas de FeriantesFavoritos
+Route::get('/ferfav','FeriantesFavorito@index');
+Route::get('/ferfav/{id}','FeriantesFavorito@show');
+Route::post('/ferfav/create','FeriantesFavorito@store');
+Route::put('/ferfav/update/{id}','FeriantesFavorito@update');
+Route::delete('/ferfav/delete/{id}','FeriantesFavorito@destroy');
+
+//Rutas de Feria_FeriaFavorito
+Route::get('/fer_ferfav','Feria_FeriaFavorito@index');
+Route::get('/fer_ferfav/{id}','Feria_FeriaFavorito@show');
+Route::post('/fer_ferfav/create','Feria_FeriaFavorito@store');
+Route::put('/fer_ferfav/update/{id}','Feria_FeriaFavorito@update');
+Route::delete('/fer_ferfav/delete/{id}','Feria_FeriaFavorito@destroy');
+
+//Rutas de Feriante_FeriantesFavoritos
+Route::get('/fer_ferifav','Feriante_FeriantesFavorito@index');
+Route::get('/fer_ferifav/{id}','Feriante_FeriantesFavorito@show');
+Route::post('/fer_ferifav/create','Feriante_FeriantesFavorito@store');
+Route::put('/fer_ferifav/update/{id}','Feriante_FeriantesFavorito@update');
+Route::delete('/fer_ferifav/delete/{id}','Feriante_FeriantesFavorito@destroy');
+
+//Rutas de FeriantesFavoritos
+Route::get('/ferifav','FeriaFavorito@index');
+Route::get('/ferifav/{id}','FeriaFavorito@show');
+Route::post('/ferifav/create','FeriaFavorito@store');
+Route::put('/ferifav/update/{id}','FeriaFavorito@update');
+Route::delete('/ferifav/delete/{id}','FeriaFavorito@destroy');
+
+//Rutas de OrdeDeDespacho
+Route::get('/OrdenDes','OrdenDeDespacho@index');
+Route::get('/OrdenDes/{id}','OrdenDeDespacho@show');
+Route::post('/OrdenDes/create','OrdenDeDespacho@store');
+Route::put('/OrdenDes/update/{id}','OrdenDeDespacho@update');
+Route::delete('/OrdenDes/delete/{id}','OrdenDeDespacho@destroy');
