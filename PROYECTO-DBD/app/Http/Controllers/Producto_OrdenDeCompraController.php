f6e89ff50bd031d1ce2c7c0f681ae5d9331e522a
javier10969 <javier.lopez@usach.cl>
2021-01-17 15:58:09 -0300

    [JAvier][Controllers correspondientes]

--- PROYECTO-DBD/app/Http/Controllers/Producto_OrdenDeCompraController.php
+++ PROYECTO-DBD/app/Http/Controllers/Producto_OrdenDeCompraController.php
@@ -14,16 +14,13 @@ class Producto_OrdenDeCompraController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $producto_ordenDeCompra = Productos_orden_de_compra::all()->where('delete',false);
+        if($producto_ordenDeCompra != NULL){
+            return response()->json($producto_ordenDeCompra);
+        }
+        return response()->json([
+            "message"=>"No se encontró el Productos_orden_de_compra solicitado",
+        ],404);
     }
 
     /**
@@ -35,6 +32,28 @@ class Producto_OrdenDeCompraController extends Controller
     public function store(Request $request)
     {
         //
+        $producto_ordenDeCompra = new Productos_orden_de_compra();
+        $validatedData = $request->validate([
+            'id_orden_compra' => ['require' , 'numeric'],
+            'id_producto' => ['require' , 'numeric']
+        ]);
+        
+        $producto = Producto::find($request->id_producto);
+        if ($producto == NULL){
+            return response()->json([
+                'message'=>'No existe usuario con esa id']);
+        }
+        $feriante = Feriante::find($request->id_feriante);
+        if ($producto_ordenDeCompra == NULL){
+            return response()->json([
+                'message'=>'No existe usuario con esa id']);
+        }
+        $producto_ordenDeCompra->save();
+        return response()->json([
+        "mesage"=>"Se ha creado una store",
+        "id" => $producto_ordenDeCompra->id
+        ],202);
+
     }
 
     /**
@@ -46,17 +65,13 @@ class Producto_OrdenDeCompraController extends Controller
     public function show($id)
     {
         //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $producto_ordenDeCompra = Productos_orden_de_compra::find($id);
+        if($producto_ordenDeCompra == NULL){
+            return response()->json([
+                'message'=>'No se encontro la orden de compra'
+            ]);
+        }
+        return response()->json($producto_ordenDeCompra);
     }
 
     /**
@@ -68,7 +83,8 @@ class Producto_OrdenDeCompraController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $producto_ordenDeCompra = Productos_orden_de_compra::find($id);
+        return response()->json($producto_ordenDeCompra);
     }
 
     /**
@@ -79,6 +95,12 @@ class Producto_OrdenDeCompraController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $producto_ordenDeCompra = Productos_orden_de_compra::find($id);
+        $producto_ordenDeCompra->delete();
+        return response()->json([
+            "message"-> "Productos_orden_de_compra elimindada",
+            "id"=>$producto_ordenDeCompra->id
+            ],201);    
+        }
     }
 }
