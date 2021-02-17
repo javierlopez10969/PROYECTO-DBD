a26ff2c36f9290951031b28fd15e883c9623a992
David Ramirez <david.ramirez.v@usach.cl>
2021-02-17 00:26:36 -0300

    [David Ramirez - Javier Lopez] - Realizando front end pagina de bienvenida

---
+++ PROYECTO-DBD/resources/views/prueba.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Prueba</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+
+    <!-- barra superior -->
+        <div class="row color1">
+            <div class="col">
+                <a href="https://ibb.co/df9mxhD"><img src="https://i.ibb.co/JcL3ghH/logo.png"  alt="logo" border="0"></a>
+            </div>
+        </div>
+     <!-- barra productos -->
+        <div class="row text-center">
+            <div class="col border border-dark">
+                <h4> Frutas </h4>
+            </div>
+
+            <div class="col">
+                <h2> Verduras </h2>
+            </div>  
+            <div class="col ">
+                <h2> Pollo </h2>
+            </div>
+
+            <div class="col">
+                <h2> Carne y Pescado </h2>
+            </div>
+
+            <div class="col ">
+                <h2> Huevos </h2> 
+            </div>
+
+            <div class="col">
+                <h2> Productos higiénicos </h2>
+            </div>  
+            <div class="col ">
+                <h2> Comida para mascotas </h2>
+            </div>
+
+            <div class="col">
+                <h2> Otros </h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col ">
+                1 of 2
+            </div>
+
+            <div class="col">
+                2 of 2
+            </div>    
+        </div>
+
+        <div class="row text-center">
+
+                <a href="https://ibb.co/4T4NDFr"><img src="https://i.ibb.co/QNYmqJw/Sin-t-tulo-1.png" alt="Frutas y verduras" border="0"></a>
+
+
+        </div>
+
+    </div>
+
+</body>
+</html>
+
+<!--CSS
+estilo de colores (pude ser HEX)   ej #FFFFFF 
+    -->
+<style> 
+    .color1{
+        background-color:#A7C957;
+        color:white;
+    }
+
+    .color2{
+        background-color:green;
+        color:red;
+    }
+
+ </style> 
