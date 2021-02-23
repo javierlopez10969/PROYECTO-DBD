d98ccd9e44ad0a2ed103e477f40d72a981f7e509
martinormazabal <martin.ormazabal@usach.cl>
2021-02-23 19:21:17 -0300

    En construcción, pasar el despacho de figma a html-css

---
+++ PROYECTO-DBD/resources/views/despacho.blade.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<!-- saved from url=(0051)https://getbootstrap.com/docs/4.0/examples/sign-in/ -->
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/despacho/">
+<title>FERION - Feria Online</title>
+</head>
+<body class="container-fluid">
+<div class="v155_14"><div class="name"></div></div>
+<div class="v155_14"><div class="name"></div></div>
+<div class="v162_14"></div><div class="v162_14"></div>
+<div class="v147_0"><div class="v147_1"></div>
+<div class="v147_2"></div><span class="v147_3">Confirmar Despacho</span></div>
+<div class="v162_14"></div>
+<div class="v147_0"><div class="v147_1"></div>
+<div class="v147_2"></div><span class="v147_3">Confirmar Despacho</span></div>
+</body>
+</html>
+<!--CSS
+estilo de colores (pude ser HEX)   ej #FFFFFF 
+    -->
+<style>
+* {
+  overflow-x: hidden;
+  padding: 0px !important;
+  margin: 0px !important;
+}
+.v155_14 {
+  width: 100%;
+  height: 1024px;
+  background: url("../images/v155_14.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.name {
+  color: #fff;
+}
+.v155_14 {
+  width: 100%;
+  height: 1024px;
+  background: url("../images/v155_14.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.name {
+  color: #fff;
+}
+.v162_14 {
+  width: 804px;
+  height: 58px;
+  background: rgba(218,126,126,1);
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v162_14 {
+  width: 804px;
+  height: 58px;
+  background: rgba(218,126,126,1);
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v147_0 {
+  width: 100%;
+  height: 157px;
+  background: url("../images/v147_0.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_1 {
+  width: 100%;
+  height: 100px;
+  background: rgba(167,201,87,1);
+  opacity: 1;
+  position: absolute;
+  top: 28px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_2 {
+  width: 378px;
+  height: 157px;
+  background: url("../images/v147_2.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_3 {
+  width: 390px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 62px;
+  left: 525px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+.v162_14 {
+  width: 804px;
+  height: 58px;
+  background: rgba(218,126,126,1);
+  padding: 10px 10px;
+  margin: 10px;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v147_0 {
+  width: 100%;
+  height: 157px;
+  background: url("../images/v147_0.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_1 {
+  width: 100%;
+  height: 100px;
+  background: rgba(167,201,87,1);
+  opacity: 1;
+  position: absolute;
+  top: 28px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_2 {
+  width: 378px;
+  height: 157px;
+  background: url("../images/v147_2.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_3 {
+  width: 390px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 62px;
+  left: 525px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+.v147_0 {
+  width: 100%;
+  height: 157px;
+  background: url("../images/v147_0.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_1 {
+  width: 100%;
+  height: 100px;
+  background: rgba(167,201,87,1);
+  opacity: 1;
+  position: absolute;
+  top: 28px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_2 {
+  width: 378px;
+  height: 157px;
+  background: url("../images/v147_2.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v147_3 {
+  width: 390px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 62px;
+  left: 525px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+</style>
