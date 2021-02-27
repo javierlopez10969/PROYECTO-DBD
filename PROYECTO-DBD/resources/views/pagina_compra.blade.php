4b4b61154e33ce0464a63f9b530ab81edbcb208e
Sebastian Astete Ramirez <sebastian.astete.r@usach.cl>
2021-02-27 18:15:15 -0300

    [Sebastian] Vista pagina compra

--- PROYECTO-DBD/resources/views/pagina_compra.blade.php
+++ PROYECTO-DBD/resources/views/pagina_compra.blade.php
@@ -25,7 +25,7 @@
                 
                 <div class="navbar-collapse collapse">
                     <div class="col-sm text-center padding-text color7">
-                        <h1> Mi carrito </h1>
+                        <h1> Comprar </h1>
                     </div>
                 </div>
 
@@ -85,17 +85,7 @@
                 <!-- Primera linea -->
                 <div class="row">
 
-                    <!-- Barrita mostrando el precio del producto -->
-                    <div class="col">
-                        <div class="row" style="padding: 30px">
-                            <div class="container-fluid ventana_precio">
-                                <div class="row">
-                                    <div class="col titulo_precio text-start"> $:</div>
-                                    <div class="col titulo_precio text-end"> 0 </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
 
                      <!-- Barrita mostrando el nombre del producto -->
                      <div class="col">
@@ -103,13 +93,36 @@
                             <div class="container-fluid ventana_nombre">
                                 <div class="row">
                                     <div class="col titulo_nombre text-start"> Nombre del Producto</div>
-                                    <div class="col titulo_nombre text-end"> . </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Barrita mostrando el precio del producto -->
+                    <div class="col">
+                        <div class="row" style="padding: 30px">
+                            <div class="container-fluid ventana_precio">
+                                <div class="row">
+                                    <div class="col titulo_precio text-start"> $</div>
+                                    <div class="col titulo_precio text-end"> 0 </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Barrita mostrando la descripcion del producto -->
+                    <div class="col">
+                        <div class="row" style="padding: 30px">
+                            <div class="container-fluid ventana_descripcion">
+                                <div class="row">
+                                    <div class="col titulo_descripcion text-start">Descripcion del Producto</div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                 </div>
+
                 <!-- Botones -->
                 <div class="row" style="padding: 20px">
                     <!-- Boton de comprar ahora -->
@@ -125,13 +138,13 @@
             </div>
         </div>
 
-        <!-- Cuadrado descripcion-->
+        <!-- Cuadrado vendedor-->
         <div class="row" style="padding: 20px">
-            <div class="container-fluid ventana_descripcion">
+            <div class="container-fluid ventana_vendedor">
                 <!-- Primera linea -->
                 <div class="row">
-                    <div class="col titulo_descripcion text-start"> Descripción del Producto</div>
-                    <div class="col titulo_descripcion text-end"> . </div>
+                    <div class="col titulo_vendedor text-start"> Datos del Vendedor</div>
+                   <!-- <div class="col titulo_descripcion text-end"> . </div>-->
                 </div>
             </div>
         </div>
@@ -169,67 +182,123 @@ estilo de colores (pude ser HEX)   ej #FFFFFF
         background-color:#A7C957;
         color: black;
     }
-    .ventana_precio{
-        width: 527px;
-        height: 109px;
-        left: 813px;
-        top: 335px;
-
-        background: #F2E8CF;
-    }
-    .ventana_descripcion{
+    .ventana_vendedor{
         position: absolute;
         width: 674px;
         height: 265px;
         left: 43px;
         top: 713px;
+        background: #F2E8CF;    
+        border-radius: 20px;    
+    }
+    .ventana_compra{
+        position: absolute;
+        width: 632px;
+        height: 824px;
+        left: 761px;
+        top: 157px;
         background: #F2E8CF;
-
-    }
-    .titulo_descripcion{  
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 40px;
-        line-height: 47px;
-        padding: 20px;
-    }
-    .titulo_precio{  
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 40px;
-        line-height: 47px;
-        padding: 20px;
+        border-radius: 20px;
     }
     .ventana_nombre{
         display: flex;
         flex-direction: row;
         align-items: flex-start;
         padding: 10px;
-
-        position: absolute;
         width: 527px;
         height: 84px;
         left: 817px;
         top: 198px;
-
         background: #DB7F7F;
         border-radius: 20px;
     }
+    .ventana_precio{
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        padding: 10px;
+
+        width: 527px;
+        height: 109px;
+        left: 813px;
+        top: 335px;
+
+        background: #F2E8CF;
+        border-radius: 20px;
+    }
+    .ventana_descripcion{
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        padding: 10px;
+
+        width: 527px;
+        height: 295px;
+        left: 813px;
+        top: 463px;
+
+        background: #F2E8CF;
+        border-radius: 20px;
+    }
+    .titulo_vendedor{
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 23px;
+        display: flex;
+        align-items: center;
+        color: #000000;
+        padding: 20px;
+    }
     .titulo_nombre{
+        position: static;
+        width: 543px;
+        height: 64px;
+        left: 10px;
+        top: 10px;
+
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 23px;
+        display: flex;
+        align-items: center;
+
+        color: #000000;
+    }
+    .titulo_precio{
+        position: static;
+        width: 543px;
+        height: 64px;
+        left: 10px;
+        top: 10px;
+
         font-family: Roboto;
         font-style: normal;
         font-weight: normal;
         font-size: 40px;
         line-height: 47px;
-        padding: 20px;        
+        display: flex;
+        align-items: center;
+
+        color: #000000;
     }
+    .titulo_descripcion{
+        position: static;
+        width: 543px;
+        height: 64px;
+        left: 10px;
+        top: 10px;
 
-    .ventana_compra{
-        width: 632px;
-        height: 824px;
-        left: 761px;
-        top: 157px;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 23px;
+        display: flex;
+        align-items: center;
 
+        color: #000000;
     }
